ef4639c11d11f9f94e650c839790a2c5a18223b1
Vincent Verapen <vincentverapen2012@gmail.com>
2023-03-21 01:02:34 -0400

    Added restaurant model

--- RestaurantRNG/RestaurantRNG/Model/Restaurant.swift
+++ RestaurantRNG/RestaurantRNG/Model/Restaurant.swift
@@ -8,12 +8,32 @@
 import Foundation
 import MapKit
 import CoreLocation
-
+import Nuke
 struct Restaurant{
-    var coordinate: CLLocationCoordinate2D
-    var restaurantName: String?
-    var rating: Int?
-    var menu: [String?]?
-    var priceSign: String?
+    let distance: Double
+    let latitude: Double
+    let longitude: Double
+    let restaurantName: String
+    let rating: Double?
+    let priceSign: String?
+    var location: [String:String]
+    let link: String?
+    let imageURL: URL
+    let isClosed: Bool
+    let category: [String:String]
+    
+    init(distance: Double, latitude: Double, longitude: Double, restaurantName: String, rating: Double?, priceSign: String?, location: [String : String], link: String?, imageURL: URL, isClosed: Bool, category: [String : String]) {
+        self.distance = distance
+        self.latitude = latitude
+        self.longitude = longitude
+        self.restaurantName = restaurantName
+        self.rating = rating
+        self.priceSign = priceSign
+        self.location = location
+        self.link = link
+        self.imageURL = imageURL
+        self.isClosed = isClosed
+        self.category = category
+    }
     
 }
