34ee928709f980f17ea73b13ef05347e00160e3d
Antonio J. Franceschi <ajfranceschi@icloud.com>
2023-03-21 02:56:54 -0400

    began implementing yelp response parsing

--- RestaurantRNG/RestaurantRNG/Model/Restaurant.swift
+++ RestaurantRNG/RestaurantRNG/Model/Restaurant.swift
@@ -10,31 +10,83 @@ import MapKit
 import CoreLocation
 import Nuke
 
-struct Restaurant{
-    let distance: Double
-    let latitude: Double
-    let longitude: Double
-    let restaurantName: String
+struct RestaurantResponse: Decodable {
+    let businesses: [Restaurant]
+    let total: Int
+    let region: [String:[String:Double]]
+}
+
+struct Restaurant: Decodable {
+    let name: String?
+    let image_url: String?
+    let is_closed: Bool?
+    let url: String?
+    let categories: [[Categories]]?
     let rating: Double?
-    let priceSign: String?
-    var location: [String:String]
-    let link: String?
-    let imageURL: URL
-    let isClosed: Bool
-    let category: [String:String]
+    var coordinates: [Coordinates]
+    let price: String?
+    let distance: Double?
+    let display_phone: String?
+    let location: [Location]?
     
-    init(distance: Double, latitude: Double, longitude: Double, restaurantName: String, rating: Double?, priceSign: String?, location: [String : String], link: String?, imageURL: URL, isClosed: Bool, category: [String : String]) {
-        self.distance = distance
-        self.latitude = latitude
-        self.longitude = longitude
-        self.restaurantName = restaurantName
-        self.rating = rating
-        self.priceSign = priceSign
-        self.location = location
-        self.link = link
-        self.imageURL = imageURL
-        self.isClosed = isClosed
-        self.category = category
+    struct Location: Decodable {
+        let address1: String?
+        let address2: String?
+        let address3: String?
+        let city: String?
+        let zip_code: String?
+        let country: String?
+        let state: String?
+        let display_address: [String]?
     }
     
+    struct Coordinates: Decodable {
+        var latitude: Double?
+        var longitude: Double?
+    }
+    
+    struct Categories: Decodable {
+        let alias:String?
+        let title:String?
+    }
+    
+//    init(distance: Double, latitude: Double, longitude: Double, restaurantName: String, rating: Double?, priceSign: String?, location: [String : String], link: String?, imageURL: URL, isClosed: Bool, category: [String : String]) {
+//        self.distance = distance
+//        self.latitude = latitude
+//        self.longitude = longitude
+//        self.restaurantName = restaurantName
+//        self.rating = rating
+//        self.priceSign = priceSign
+//        self.location = location
+//        self.link = link
+//        self.imageURL = imageURL
+//        self.isClosed = isClosed
+//        self.category = category
+//    }
+    
+}
+
+extension Restaurant {
+    init(from service: Restaurant) {
+        name = service.name
+        image_url = service.image_url
+        is_closed = service.is_closed
+        url = service.url
+        rating = service.rating
+        price = service.price
+        distance = service.distance
+        display_phone = service.display_phone
+        location = [Location]()
+        coordinates = [Coordinates]()
+        categories = [[Categories]]()
+        
+        for coordinate in service.coordinates {
+            let item = Coordinates(latitude: coordinate.latitude, longitude: coordinate.longitude)
+            coordinates.append(item)
+        }
+        
+        // Location
+        
+        // Categories
+    }
 }
