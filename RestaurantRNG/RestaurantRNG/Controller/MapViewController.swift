cf989f4984fe9cdecebdb071effbb023e369e427
Antonio J. Franceschi <ajfranceschi@icloud.com>
2023-03-22 02:43:01 -0400

    implemented YelpAPI call restaurant parsing.  Modified YelpAPI library to include price and distance

--- RestaurantRNG/RestaurantRNG/Controller/MapViewController.swift
+++ RestaurantRNG/RestaurantRNG/Controller/MapViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import MapKit
+import YelpAPI
 
 class MapViewController: UIViewController {
     
@@ -27,7 +28,9 @@ class MapViewController: UIViewController {
     
     var restaurants = [Restaurant]() {
         didSet {
-            updateMapView()
+            DispatchQueue.main.async {
+                self.updateMapView()
+            }
         }
     }
     
@@ -53,8 +56,12 @@ class MapViewController: UIViewController {
         mapConfiguration.pointOfInterestFilter = MKPointOfInterestFilter.excludingAll
         mapView.preferredConfiguration = mapConfiguration
         
+        
+        
+        
         DispatchQueue.main.async {
             self.updateMapView()
+            self.updateRestaurants()
         }
     }
     
@@ -65,9 +72,7 @@ class MapViewController: UIViewController {
         // this should eventually be adjusted based on search radius selected by user
         let region = MKCoordinateRegion(center: currentLocation.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
         mapView.setRegion(region, animated: false)
-        
-        updateRestaurants()
-        
+                
         // "for each" loop that effectively adds a pin/annotation for each restaurant in array
         for restaurant in restaurants {
             let annotation = MKPointAnnotation()
@@ -124,7 +129,7 @@ extension MapViewController : CLLocationManagerDelegate {
             DispatchQueue.main.async {
                 // self.updateRestaurants() // will probably just be able to just call this because it will call
                 // updateMapView() itself
-                self.updateMapView()
+//                self.updateMapView()
             }
         }
     }
@@ -140,5 +145,67 @@ extension MapViewController : CLLocationManagerDelegate {
 extension MapViewController : MKMapViewDelegate {
     
     
+}
+
+
+// MARK: Get Restaurants from yelp function
+extension MapViewController {
+    func getRestaurantsFromYelp(latitude: Double, longitude: Double){
+        guard (latitude >= -90 && latitude <= 90) else {
+            print("latitude must be between -90 and 90")
+            return
+        }
+        
+        guard (longitude >= -180 && longitude <= 180) else {
+            print("latitude must be between -180 and 180")
+            return
+        }
+        
+        // MARK: Yelp API Test
+        let yelpClient = YLPClient(apiKey: YELP_API_KEY)
+        let yelpCoordinate = YLPCoordinate(latitude: latitude, longitude: longitude)
+        let yelpQuery = YLPQuery(coordinate: yelpCoordinate)
+        yelpQuery.categoryFilter = ["restaurants"]
+        yelpQuery.radiusFilter = 1600
+        yelpQuery.limit = 20
+
+        
+        yelpClient.search(with: yelpQuery) { search, error in
+            if let error = error {
+                print("Could not complete search. Error: \(error)")
+            } else {
+                if let search = search {
+                    for business in search.businesses {
+                        var stAddress2 = ""
+                        if business.location.address.count > 1 {
+                            stAddress2 = business.location.address[1]
+                        }
+                        self.restaurants.append(Restaurant(
+                            name: business.name,
+                            image_url: business.imageURL,
+                            is_closed: business.isClosed,
+                            url: business.url, category: business.categories[0].name,
+                            rating: business.rating,
+                            latitude: business.location.coordinate!.latitude,
+                            longitude: business.location.coordinate!.longitude,
+                            price: business.price,
+                            distance: business.distance,
+                            display_phone: business.phone,
+                            streetAddress1: business.location.address[0],
+                            streetAddress2: stAddress2,
+                            zipCode: business.location.postalCode,
+                            city: business.location.city,
+                            country: business.location.countryCode,
+                            state: business.location.stateCode))
+                    }
+                }
+            }
+            for restaurant in self.restaurants {
+                restaurant.toString()
+            }
+        }
+        
+        
+    }
 }
 
--- RestaurantRNG/RestaurantRNG/Util/HelperMethods.swift
+++ RestaurantRNG/RestaurantRNG/Util/HelperMethods.swift
@@ -1,62 +1,70 @@
+////
+////  HelperMethods.swift
+////  RestaurantRNG
+////
+////  Created by Antonio Franceschi on 3/21/23.
+////
 //
-//  HelperMethods.swift
-//  RestaurantRNG
+//import Foundation
+//import YelpAPI
 //
-//  Created by Antonio Franceschi on 3/21/23.
+//// YELP REQUEST
 //
-
-import Foundation
-
-// YELP REQUEST
-
-func getRestaurantsFromYelp(latitude: Double, longitude: Double) {
-    var restaurants = [Restaurant]()
-    
-    guard (latitude >= -90 && latitude <= 90) else {
-        print("latitude must be between -90 and 90")
-        return
-    }
-    
-    guard (longitude >= -180 && longitude <= 180) else {
-        print("latitude must be between -180 and 180")
-        return
-    }
-    
-    
-    let radius = 1600 // meters ~ 1 mile
-    let category = "restaurants"
-    let limit = 20
-    
-    let request = NSMutableURLRequest(url: NSURL(string: "\(YELP_API_BASE_URL)/businesses/search?latitude=\(latitude)&longitude=\(longitude)&radius=\(radius)&categories=\(category)&sort_by=distance&device_platform=ios&limit=\(limit)")! as URL,
-                                             cachePolicy: .useProtocolCachePolicy,
-                                             timeoutInterval: 10.0)
-    request.httpMethod = "GET"
-    request.allHTTPHeaderFields = YELP_HEADERS
-    
-    // Session
-    let session = URLSession.shared
-    session.dataTask(with: request as URLRequest) { data, response, error in
-        if error != nil {
-            print(error as Any)
-        } else {
-            guard let jsonData = data else {
-                print("Data is nil")
-                return
-            }
-            
-            do {
-                let decoder = JSONDecoder()
-                let jsonResponse = try decoder.decode([RestaurantResponse].self, from: jsonData)
-                
-                let businesses = jsonResponse[0].map {Restaurant(from: $0)} //root of business
-                let restaurants = businesses.map {Restaurant(from: $0)}
-                    
-                }
-                
-            } catch let error {
-                print("Error Parsing JSON: \(error.localizedDescription)")
-            }
-        }
-    }.resume()
-    
-}
+//func getRestaurantsFromYelp(latitude: Double, longitude: Double){
+//    guard (latitude >= -90 && latitude <= 90) else {
+//        print("latitude must be between -90 and 90")
+//        return
+//    }
+//    
+//    guard (longitude >= -180 && longitude <= 180) else {
+//        print("latitude must be between -180 and 180")
+//        return
+//    }
+//    
+//    // MARK: Yelp API Test
+//    let yelpClient = YLPClient(apiKey: YELP_API_KEY)
+//    let yelpCoordinate = YLPCoordinate(latitude: latitude, longitude: longitude)
+//    let yelpQuery = YLPQuery(coordinate: yelpCoordinate)
+//    yelpQuery.categoryFilter = ["restaurants"]
+//    yelpQuery.radiusFilter = 1600
+//    yelpQuery.limit = 20
+//
+//    
+//    yelpClient.search(with: yelpQuery) { search, error in
+//        if let error = error {
+//            print("Could not complete search. Error: \(error)")
+//        } else {
+//            if let search = search {
+//                print(search.businesses.count)
+//                for business in search.businesses {
+//                    var stAddress2 = ""
+//                    if business.location.address.count > 1 {
+//                        stAddress2 = business.location.address[1]
+//                    }
+//                    MapViewController.restaurants.append(Restaurant(
+//                        name: business.name,
+//                        image_url: business.imageURL,
+//                        is_closed: business.isClosed,
+//                        url: business.url, category: business.categories[0].name,
+//                        rating: business.rating,
+//                        latitude: business.location.coordinate!.latitude,
+//                        longitude: business.location.coordinate!.longitude,
+//                        price: business.price,
+//                        distance: business.distance,
+//                        display_phone: business.phone,
+//                        streetAddress1: business.location.address[0],
+//                        streetAddress2: stAddress2,
+//                        zipCode: business.location.postalCode,
+//                        city: business.location.city,
+//                        country: business.location.countryCode,
+//                        state: business.location.stateCode))
+//                }
+//            }
+//        }
+//        for restaurant in MapViewController.restaurants {
+//            restaurant.toString()
+//        }
+//    }
+//    
+//    
+//}
