2fa849b1011808c9da27f16baef88efb403e615b
Vincent Verapen <vincentverapen2012@gmail.com>
2023-03-28 23:53:44 -0400

    added restaurant view controller features

--- RestaurantRNG/RestaurantRNG/Controller/RestaurantViewController.swift
+++ RestaurantRNG/RestaurantRNG/Controller/RestaurantViewController.swift
@@ -6,13 +6,29 @@
 //
 
 import UIKit
-
+import Nuke
 class RestaurantViewController: UIViewController {
 
+    @IBOutlet weak var restaurantImageView: UIImageView!
+    @IBOutlet weak var restaurantLinkLabel: UILabel!
+    @IBOutlet weak var restaurantAddressLabel: UILabel!
+    @IBOutlet weak var restuarantPhoneNumber: UILabel!
+    @IBOutlet weak var restaurantPriceLabel: UILabel!
+    @IBOutlet weak var restaurantNameLabel: UILabel!
+    @IBOutlet weak var restaurantRatingLabel: UILabel!
+    var restaurant:Restaurant!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        Nuke.loadImage(with: restaurant.image_url!, into: restaurantImageView)
+        restaurantNameLabel.text = restaurant.name
+        let text:String = String(format: "%f", restaurant.rating)
+        restaurantRatingLabel.text = text
+        restaurantAddressLabel.text = restaurant.streetAddress1
+        restuarantPhoneNumber.text = restaurant.display_phone
+        restaurantPriceLabel.text = restaurant.price
     }
     
 
