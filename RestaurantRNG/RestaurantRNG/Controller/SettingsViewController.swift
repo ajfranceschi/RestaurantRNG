1958717f869c43a71cb4ea9eea2e925c93001200
Antonio J. Franceschi <ajfranceschi@icloud.com>
2023-03-29 01:07:12 -0400

    Connected settings UI to SettingsVC

--- RestaurantRNG/RestaurantRNG/Controller/SettingsViewController.swift
+++ RestaurantRNG/RestaurantRNG/Controller/SettingsViewController.swift
@@ -8,14 +8,31 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-
+    @IBOutlet weak var distanceSlider: UISlider!
+    @IBOutlet weak var ratingSlider: UISlider!
+    @IBOutlet weak var priceSlider: UISlider!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func didChangeDistanceSliderValue(_ sender: UISlider) {
+        
+    }
+    
+    
+    @IBAction func didChangeRatingSliderValue(_ sender: UISlider) {
+    }
+    
+    @IBAction func didChangePriceSliderValue(_ sender: UISlider) {
+    }
+    
+    
     @IBAction func didPressBackButton(_ sender: UIButton) {
         self.dismiss(animated: true)
     }
