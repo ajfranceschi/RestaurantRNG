2e062585f44c735f85ce4f16714f842c1dd26bbd
Antonio J. Franceschi <ajfranceschi@icloud.com>
2023-04-02 00:15:07 -0400

    Settings page: connected Sliders with lables

--- RestaurantRNG/RestaurantRNG/Controller/SettingsViewController.swift
+++ RestaurantRNG/RestaurantRNG/Controller/SettingsViewController.swift
@@ -15,27 +15,46 @@ class SettingsViewController: UIViewController {
     @IBOutlet weak var ratingLabel: UILabel!
     @IBOutlet weak var priceLabel: UILabel!
     
+    var distance: Float = 5.0
+    var rating: Float = 0.0
+    var price: Int = 5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        ratingSlider.setValue(rating, animated: true)
+        
+        
     }
     
     @IBAction func didChangeDistanceSliderValue(_ sender: UISlider) {
-        
+        distanceLabel.text = String(format: "%.1f mi", sender.value)
+        distance = (String(format: "%.1f", sender.value) as NSString).floatValue
     }
     
     
     @IBAction func didChangeRatingSliderValue(_ sender: UISlider) {
+        ratingLabel.text = String(format: "%.1f", sender.value)
+        rating = (String(format: "%.1f", sender.value) as NSString).floatValue
     }
     
     @IBAction func didChangePriceSliderValue(_ sender: UISlider) {
+        price = Int(floor(sender.value))
+        var priceString = ""
+        for _ in 1...price {
+            priceString = priceString + "$"
+        }
+        
+        priceLabel.text = priceString
     }
     
     
     @IBAction func didPressBackButton(_ sender: UIButton) {
         self.dismiss(animated: true)
     }
+    
+    
     /*
     // MARK: - Navigation
 
